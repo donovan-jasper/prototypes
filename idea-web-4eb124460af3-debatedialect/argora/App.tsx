8fb55660f9fad153195f66da5617253ed2e615b6
donovan-jasper <donovan@insecurity.dev>
2026-03-18 01:51:46 +0000

    improve: Create App.tsx as the entry point that renders DebateScreen  [improved]

---
+++ idea-web-4eb124460af3-debatedialect/argora/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import DebateScreen from './app/screens/DebateScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Debate" 
+          component={DebateScreen}
+          options={{ title: 'Argora' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
