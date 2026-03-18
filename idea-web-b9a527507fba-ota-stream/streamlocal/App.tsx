9cf8c72863f170256d962c5a54850af94f9372e9
donovan-jasper <donovan@insecurity.dev>
2026-03-18 19:19:24 +0000

    improve: Wrap the bottom tab navigator in a stack navigator and add P [improved]

--- idea-web-b9a527507fba-ota-stream/streamlocal/App.tsx
+++ idea-web-b9a527507fba-ota-stream/streamlocal/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Provider as PaperProvider } from 'react-native-paper';
 import HomeScreen from './src/screens/HomeScreen';
 import FavoritesScreen from './src/screens/FavoritesScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
+import PlayerScreen from './src/screens/PlayerScreen';
 import { AppProvider } from './src/context/AppContext';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Favorites" component={FavoritesScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <AppProvider>
       <PaperProvider>
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Favorites" component={FavoritesScreen} />
-            <Tab.Screen name="Settings" component={SettingsScreen} />
-          </Tab.Navigator>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name="Main" 
+              component={TabNavigator} 
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen 
+              name="Player" 
+              component={PlayerScreen}
+              options={{ presentation: 'modal' }}
+            />
+          </Stack.Navigator>
         </NavigationContainer>
       </PaperProvider>
     </AppProvider>
