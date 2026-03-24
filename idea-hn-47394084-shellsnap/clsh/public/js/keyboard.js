6f5aae71089f0a517d95a096d6428523d8021864
donovan-jasper <donovan@insecurity.dev>
2026-03-24 17:28:21 +0000

    improve: Enhance the `handleKey` function in `clsh/public/js/keyboard [improved]

--- idea-hn-47394084-shellsnap/clsh/public/js/keyboard.js
+++ idea-hn-47394084-shellsnap/clsh/public/js/keyboard.js
@@ -37,35 +37,52 @@ function handleKeyPress(e) {
 function handleKey(key) {
     let data = '';
     
-    switch(key) {
-        case 'Escape':
-            data = '\x1b'; // ESC character
-            break;
-        case 'Tab':
-            data = '\t';
-            break;
-        case 'Control':
-            isCtrlPressed = !isCtrlPressed;
-            updateModifierDisplay();
-            return;
-        case 'Alt':
-            isAltPressed = !isAltPressed;
-            updateModifierDisplay();
-            return;
-        case 'ArrowLeft':
-            data = '\x1b[D';
-            break;
-        case 'ArrowUp':
-            data = '\x1b[A';
-            break;
-        case 'ArrowDown':
-            data = '\x1b[B';
-            break;
-        case 'ArrowRight':
-            data = '\x1b[C';
-            break;
-        default:
-            data = key;
+    if (isCtrlPressed && key.length === 1 && key.match(/[a-z]/i)) {
+        // Convert character to ASCII control character
+        const charCode = key.charCodeAt(0);
+        if (charCode >= 97 && charCode <= 122) { // a-z
+            data = String.fromCharCode(charCode - 96); // a -> \x01, b -> \x02, etc.
+        } else if (charCode >= 65 && charCode <= 90) { // A-Z
+            data = String.fromCharCode(charCode - 64); // A -> \x01, B -> \x02, etc.
+        }
+        isCtrlPressed = false; // Release Ctrl modifier
+        updateModifierDisplay();
+    } else if (isAltPressed) {
+        // Prepend ESC character
+        data = '\x1b' + key;
+        isAltPressed = false; // Release Alt modifier
+        updateModifierDisplay();
+    } else {
+        switch(key) {
+            case 'Escape':
+                data = '\x1b'; // ESC character
+                break;
+            case 'Tab':
+                data = '\t';
+                break;
+            case 'Control':
+                isCtrlPressed = !isCtrlPressed;
+                updateModifierDisplay();
+                return;
+            case 'Alt':
+                isAltPressed = !isAltPressed;
+                updateModifierDisplay();
+                return;
+            case 'ArrowLeft':
+                data = '\x1b[D';
+                break;
+            case 'ArrowUp':
+                data = '\x1b[A';
+                break;
+            case 'ArrowDown':
+                data = '\x1b[B';
+                break;
+            case 'ArrowRight':
+                data = '\x1b[C';
+                break;
+            default:
+                data = key;
+        }
     }
     
     if (currentSessionId && terminals[currentSessionId]) {
