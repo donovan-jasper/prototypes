ce30ac7c49fce723b6c1376c5141ce701898baca
donovan-jasper <donovan@insecurity.dev>
2026-04-02 06:54:38 +0000

    improve: Implement a basic Express server in server.js that serves st [improved]

---
+++ idea-hn-47393510-flipfile/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: true,
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res, next) => {
+  res.header('Cross-Origin-Embedder-Policy', 'require-corp');
+  res.header('Cross-Origin-Opener-Policy', 'same-origin');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
